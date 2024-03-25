d8195dcff2e3c64ea16b94f80230cf3801767065
chenhao <chenhaomails@gmail.com>
2024-03-25 09:18:41 +0800

    + run batch script for examples/noisy_ekg_hrv_ge

---
+++ examples/noisy_ekg_hrv_ge/main.sh
@@ -0,0 +1,101 @@
+#cp ../../Case101.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > Case101.segment.list; sh clean.sh; rm Casexxxx
+# mv examples/noisy_ekg_hrv_ge/Case101.dat . 
+# mv examples/noisy_ekg_hrv_ge/Case101.hea . 
+# python step5_segment_plot.py Case101.segment.list 3
+
+#cp ../../Case105.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case105.segment.list  ; sh clean.sh;    rm Case105.*              
+#cp ../../Case110.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case110.segment.list  ; sh clean.sh;    rm Case110.*              
+cp ../../Case111.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case111.segment.list  ; sh clean.sh;    rm Case111.*              
+cp ../../Case113.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case113.segment.list  ; sh clean.sh;    rm Case113.*              
+cp ../../Case118.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case118.segment.list  ; sh clean.sh;    rm Case118.*              
+cp ../../Case128.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case128.segment.list  ; sh clean.sh;    rm Case128.*              
+cp ../../Case133.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case133.segment.list  ; sh clean.sh;    rm Case133.*              
+cp ../../Case139.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case139.segment.list  ; sh clean.sh;    rm Case139.*              
+cp ../../Case142.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case142.segment.list  ; sh clean.sh;    rm Case142.*              
+cp ../../Case143.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case143.segment.list  ; sh clean.sh;    rm Case143.*              
+cp ../../Case152.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case152.segment.list  ; sh clean.sh;    rm Case152.*              
+cp ../../Case157.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case157.segment.list  ; sh clean.sh;    rm Case157.*              
+cp ../../Case16.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case16.segment.list ; sh clean.sh;      rm Case16.*              
+cp ../../Case173.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case173.segment.list  ; sh clean.sh;    rm Case173.*              
+cp ../../Case175.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case175.segment.list  ; sh clean.sh;    rm Case175.*              
+cp ../../Case177.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case177.segment.list  ; sh clean.sh;    rm Case177.*              
+cp ../../Case180.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case180.segment.list  ; sh clean.sh;    rm Case180.*              
+cp ../../Case183.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case183.segment.list  ; sh clean.sh;    rm Case183.*              
+cp ../../Case186.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case186.segment.list  ; sh clean.sh;    rm Case186.*              
+cp ../../Case191.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case191.segment.list  ; sh clean.sh;    rm Case191.*              
+cp ../../Case199.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case199.segment.list  ; sh clean.sh;    rm Case199.*              
+cp ../../Case204.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case204.segment.list  ; sh clean.sh;    rm Case204.*              
+cp ../../Case208.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case208.segment.list  ; sh clean.sh;    rm Case208.*              
+cp ../../Case209.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case209.segment.list  ; sh clean.sh;    rm Case209.*              
+cp ../../Case219.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case219.segment.list  ; sh clean.sh;    rm Case219.*              
+cp ../../Case226.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case226.segment.list  ; sh clean.sh;    rm Case226.*              
+cp ../../Case23.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case23.segment.list ; sh clean.sh;      rm Case23.*              
+cp ../../Case235.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case235.segment.list  ; sh clean.sh;    rm Case235.*              
+cp ../../Case249.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case249.segment.list  ; sh clean.sh;    rm Case249.*              
+cp ../../Case25.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case25.segment.list ; sh clean.sh;      rm Case25.*              
+cp ../../Case253.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case253.segment.list  ; sh clean.sh;    rm Case253.*              
+cp ../../Case257.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case257.segment.list  ; sh clean.sh;    rm Case257.*              
+cp ../../Case263.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case263.segment.list  ; sh clean.sh;    rm Case263.*              
+cp ../../Case274.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case274.segment.list  ; sh clean.sh;    rm Case274.*              
+cp ../../Case279.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case279.segment.list  ; sh clean.sh;    rm Case279.*              
+cp ../../Case280.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case280.segment.list  ; sh clean.sh;    rm Case280.*              
+cp ../../Case292.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case292.segment.list  ; sh clean.sh;    rm Case292.*              
+cp ../../Case305.* . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case305.segment.list  ; sh clean.sh;    rm Case305.*              
+cp ../../Case36.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case36.segment.list ; sh clean.sh;      rm Case36.*              
+cp ../../Case46.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case46.segment.list ; sh clean.sh;      rm Case46.*              
+cp ../../Case47.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case47.segment.list ; sh clean.sh;      rm Case47.*              
+cp ../../Case54.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case54.segment.list ; sh clean.sh;      rm Case54.*              
+cp ../../Case60.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case60.segment.list ; sh clean.sh;      rm Case60.*              
+cp ../../Case68.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case68.segment.list ; sh clean.sh;      rm Case68.*              
+cp ../../Case74.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case74.segment.list ; sh clean.sh;      rm Case74.*              
+cp ../../Case75.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case75.segment.list ; sh clean.sh;      rm Case75.*              
+cp ../../Case95.*  . ;python step1_noise_spike_detect.py; python step2_nn_intervals_gen.py; python step3_nn_hist_analysis.py; python step4_potential_arrhy.py > segment.Case95.segment.list ; sh clean.sh;      rm Case95.*              
+
+#cp segment.Case105.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case105.segment.list 1;
+#cp segment.Case110.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case110.segment.list 1; cd -; 
+
+#cp segment.Case111.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case111.segment.list 1; cd -; 
+#cp segment.Case113.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case113.segment.list 1; cd -; 
+#cp segment.Case118.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case118.segment.list 1; cd -; 
+#cp segment.Case128.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case128.segment.list 1; cd -; 
+#cp segment.Case133.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case133.segment.list 1; cd -; 
+#cp segment.Case139.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case139.segment.list 1; cd -; 
+#cp segment.Case142.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case142.segment.list 1; cd -; 
+#cp segment.Case143.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case143.segment.list 1; cd -; 
+#cp segment.Case152.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case152.segment.list 1; cd -; 
+#cp segment.Case157.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case157.segment.list 1; cd -; 
+#cp segment.Case16.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case16.segment.list  1; cd -; 
+#cp segment.Case173.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case173.segment.list 1; cd -; 
+#cp segment.Case175.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case175.segment.list 1; cd -; 
+#cp segment.Case177.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case177.segment.list 1; cd -; 
+#cp segment.Case180.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case180.segment.list 1; cd -; 
+#cp segment.Case183.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case183.segment.list 1; cd -; 
+#cp segment.Case186.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case186.segment.list 1; cd -; 
+#cp segment.Case191.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case191.segment.list 1; cd -; 
+#cp segment.Case199.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case199.segment.list 1; cd -; 
+#cp segment.Case204.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case204.segment.list 1; cd -; 
+#cp segment.Case208.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case208.segment.list 1; cd -; 
+#cp segment.Case209.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case209.segment.list 1; cd -; 
+#cp segment.Case219.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case219.segment.list 1; cd -; 
+#cp segment.Case226.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case226.segment.list 1; cd -; 
+#cp segment.Case23.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case23.segment.list  1; cd -; 
+#cp segment.Case235.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case235.segment.list 1; cd -; 
+#cp segment.Case249.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case249.segment.list 1; cd -; 
+#cp segment.Case25.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case25.segment.list  1; cd -; 
+#cp segment.Case253.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case253.segment.list 1; cd -; 
+#cp segment.Case257.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case257.segment.list 1; cd -; 
+#cp segment.Case263.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case263.segment.list 1; cd -; 
+#cp segment.Case274.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case274.segment.list 1; cd -; 
+#cp segment.Case279.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case279.segment.list 1; cd -; 
+#cp segment.Case280.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case280.segment.list 1; cd -; 
+#cp segment.Case292.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case292.segment.list 1; cd -; 
+#cp segment.Case305.segment.list ../../   ; cd ../../;  python step5_segment_plot.py  segment.Case305.segment.list 1; cd -; 
+#cp segment.Case36.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case36.segment.list  1; cd -; 
+#cp segment.Case46.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case46.segment.list  1; cd -; 
+#cp segment.Case47.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case47.segment.list  1; cd -; 
+#cp segment.Case54.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case54.segment.list  1; cd -; 
+#cp segment.Case60.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case60.segment.list  1; cd -; 
+#cp segment.Case68.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case68.segment.list  1; cd -; 
+#cp segment.Case74.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case74.segment.list  1; cd -; 
+#cp segment.Case75.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case75.segment.list  1; cd -; 
+#cp segment.Case95.segment.list ../../    ; cd ../../;  python step5_segment_plot.py  segment.Case95.segment.list  1; cd -; 
